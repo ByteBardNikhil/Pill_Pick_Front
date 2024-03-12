a7be8571c0e6c25992e24566f8985faddb249ea7
Nikhil-197 <mahajannikhil177@gmail.com>
2024-03-12 13:43:01 +0530

    Navbar Created

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div class="logo">Logo</div>
+            <div class="search">
+                <input type="text" placeholder="Search" />
+                    <span>Pin Code:</span>
+                    <input type="text" placeholder="Enter Pin"/>
+                    </div>
+                    <div class="buttons">
+                        <button>Upload</button>
+                        <button>Login/Sign Up</button>
+                    </div>
+                </nav>
+                )
+}
+
+                export default NavBar
