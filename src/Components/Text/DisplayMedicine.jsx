a9748c7eb725ce4102edf8ef875e7dfdd3bb1481
Vansh <vanshnavgotri07@gmail.com>
2024-04-23 21:01:52 +0530

    excell

--- src/Components/Text/DisplayMedicine.jsx
+++ src/Components/Text/DisplayMedicine.jsx
@@ -21,7 +21,7 @@ const ExcelData = () => {
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
         const parsedData = XLSX.utils.sheet_to_json(sheet);
-        setMedicineData();
+        setMedicineData(parsedData);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -38,7 +38,7 @@ const ExcelData = () => {
   };
 
   return (
-    <div className="container">
+    <div className="contain">
       <div className="table-wrapper">
         <table className="table">
           <thead>
